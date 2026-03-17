dc94b593a52e4183c8b94ee5bafae7a6473f568a
lakshmimahaa <mahaapanneer@gmail.com>
2026-03-17 17:51:53 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
